cb8ac35482c9c1dd252d398519d8aa76e106442d
Nicholas Defin J. <mayagareyuki12@gmail.com>
2025-07-02 21:43:05 +0700

    Perbaikan Database Batches dan UI Upload Batch Page

--- database/migrations/2025_07_01_121554_create_batches_table.php
+++ database/migrations/2025_07_01_121554_create_batches_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('batches', function (Blueprint $table) {
             $table->id('batchid');
-            $table->string('dateofactivity');
+            $table->date('dateofactivity');
             $table->integer('treesplanted');
             $table->string('batchdate');
             $table->string('batchproof');
